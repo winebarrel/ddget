95265fd0abe9678d3fcee6478cb8f7b01b80c2aa
winebarrel <sgwr_dts@yahoo.co.jp>
2016-11-15 02:58:15 +0900

    Update homebrew shasum

--- homebrew/ddget.rb
+++ homebrew/ddget.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class Ddget < Formula
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 
   homepage 'https://github.com/winebarrel/ddget'
   url "https://github.com/winebarrel/ddget/releases/download/v#{VERSION}/ddget-v#{VERSION}-darwin-amd64.gz"
-  sha256 '05f8c34b40d67c3cb25befff6acb762120c34dc60448657280dfa55fa0e469cb'
+  sha256 'd5dc1c8f58d2bb6d5590c3c0cf877544f32851a58614c27c93b2be929c9fe126'
   version VERSION
   head 'https://github.com/winebarrel/ddget.git', :branch => 'master'
 
